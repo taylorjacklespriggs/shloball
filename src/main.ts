1ab0bfcee6b725b1792c1671632ab5fd67cdd733
sky <skylar@wayscript.com>
2023-04-16 00:32:04 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -44,22 +44,25 @@ window.onload = () => {
 
   const boundaryWidth = config.boundary.width;
 
-  const player1 = new Player(world, 40, config.player.height + 2 * boundaryWidth, 1);
-  const player2 = new Player(world, world.width - Player.WIDTH - 40, config.player.height + 2 * boundaryWidth, 2);
+  const player1 = new Player(world, 4 * boundaryWidth, config.player.height + 2 * boundaryWidth, 1);
+  const player2 = new Player(world, world.width - Player.WIDTH - 4 * boundaryWidth, config.player.height + 2 * boundaryWidth, 2);
   const ball = new Ball(world, world.width / 2 - 15, world.width / 2 - 15);
 
   // Create goals
   const goalHeight = config.goal.height;
   const goalWidth = config.goal.width;
   const goalY = (world.height - goalHeight) / 2;
-  const leftGoal = new Goal(world, boundaryWidth, goalY, goalWidth, goalHeight, 1);
-  const rightGoal = new Goal(world, world.width - goalWidth - boundaryWidth, goalY, goalWidth, goalHeight, 2);
+  const leftGoal = new Goal(world, 0, goalY, goalWidth, goalHeight, 1);
+  const rightGoal = new Goal(world, world.width - goalWidth, goalY, goalWidth, goalHeight, 2);
 
   // Create boundaries
   const topBoundary = new PhysicsObject(world, 0, 0, world.height - boundaryWidth, world.width, boundaryWidth, true);
   const bottomBoundary = new PhysicsObject(world, 0, 0, 0, world.width, boundaryWidth, true);
-  const leftBoundary = new PhysicsObject(world, 0, 0, 0, boundaryWidth, world.height, true);
-  const rightBoundary = new PhysicsObject(world, 0, world.width - boundaryWidth, 0, boundaryWidth, world.height, true);
+  const goalBoundaryHeight = (world.height - 2 * boundaryWidth - goalHeight) / 2;
+  const leftBottomBoundary = new PhysicsObject(world, 0, 0, boundaryWidth, 3 * boundaryWidth, (world.height - 2 * boundaryWidth - goalHeight) / 2, true);
+  const leftTopBoundary = new PhysicsObject(world, 0, 0, world.height - boundaryWidth - goalBoundaryHeight, 3 * boundaryWidth, (world.height - 2 * boundaryWidth - goalHeight) / 2, true);
+  const rightBottomBoundary = new PhysicsObject(world, 0, world.width - 3 * boundaryWidth, boundaryWidth, 3 * boundaryWidth, (world.height - 2 * boundaryWidth - goalHeight) / 2, true);
+  const rightTopBoundary = new PhysicsObject(world, 0, world.width - 3 * boundaryWidth, world.height - boundaryWidth - goalBoundaryHeight, 3 * boundaryWidth, (world.height - 2 * boundaryWidth - goalHeight) / 2, true);
 
   objects.push(player1);
   objects.push(player2);
@@ -70,8 +73,10 @@ window.onload = () => {
 
   objects.push(topBoundary);
   objects.push(bottomBoundary);
-  objects.push(leftBoundary);
-  objects.push(rightBoundary);
+  objects.push(leftTopBoundary);
+  objects.push(leftBottomBoundary);
+  objects.push(rightTopBoundary);
+  objects.push(rightBottomBoundary);
 
   let lastTimestamp = 0;
   let accumulator = 0;
