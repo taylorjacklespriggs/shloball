6b39e4335c38ddb8a326baf15ed35b0774e2738a
Taylor Jackle Spriggs <taylor@sigopt.com>
2023-04-15 23:23:24 -0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// webpack.config.js
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
